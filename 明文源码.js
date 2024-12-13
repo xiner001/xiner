aeda9a919dea1be3b86300b780233798963a3f56
xiner001 <xiner00001@gmail.com>
2024-12-14 03:55:03 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '13b88827-8139-413b-bde2-eab8b6b958ab';
+let userID = '9cb70044-7486-47d7-9044-9ffd4b445188';
 
-const proxyIPs = ['152.70.117.174','45.62.113.176','172.96.203.11','192.243.119.121','199.34.228.161'];
+const proxyIPs = ['165.232.129.138','143.244.210.43','199.34.228.131','199.34.228.47','199.34.228.145'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
